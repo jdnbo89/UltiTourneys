b305e67e281f36cd0612f8922c9db08a02d150d1
jdnbo89 <71927807+jdnbo89@users.noreply.github.com>
2023-07-21 11:14:12 -0700

    Fill tournament class

--- UltiTourneysApp/UltiTourneysApp/Classes/Tournament.cs
+++ UltiTourneysApp/UltiTourneysApp/Classes/Tournament.cs
@@ -3,8 +3,18 @@ namespace UltiTourneysApp.Classes
 {
 	public class Tournament
 	{
-		public Tournament()
+		public int tournamentID { get; private set; }
+		public string name { get; private set; }
+        public DateTime date { get; private set; }
+
+		public string? city { get; private set; }    // TODO: update location to a location-based class
+		public string? state { get; private set; }   // TODO: update location to a location-based class
+		public string? country { get; private set; } // TODO: update location to a location-based class
+
+        public Tournament(string name, DateTime date)
 		{
+			this.name = name;
+			this.date = date;
 		}
 	}
 }
