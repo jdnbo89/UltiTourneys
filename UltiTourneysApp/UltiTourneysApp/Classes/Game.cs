83847d1e0762b2b11f34d3130baab643d6659a43
jdnbo89 <71927807+jdnbo89@users.noreply.github.com>
2023-07-21 17:24:10 -0700

    Add Game class and TeamScore enum Add camel_case style to editorconfig

---
+++ UltiTourneysApp/UltiTourneysApp/Classes/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UltiTourneysApp.Structs;
+
+namespace UltiTourneysApp.Classes
+{
+    public class Game
+    {
+        public TeamScore teamScore1 { get; private set; }
+        public TeamScore teamScore2 { get; private set; }
+
+        public Game()
+        {
+
+        }
+    }
+}
+
