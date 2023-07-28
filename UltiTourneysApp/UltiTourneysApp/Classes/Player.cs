a3b4e86b8f25baeae40dae4bdf9b904d3ef98b01
jdnbo89 <71927807+jdnbo89@users.noreply.github.com>
2023-07-27 17:21:48 -0700

    Add email and DOB to player

--- UltiTourneysApp/UltiTourneysApp/Classes/Player.cs
+++ UltiTourneysApp/UltiTourneysApp/Classes/Player.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace UltiTourneysApp.Classes
 {
     public class Player
     {
         public int PlayerID { get; set; }
         public string Name { get; set; }
+
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Please enter a valid e-mail address")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
     }
 }
 
---
+++ UltiTourneysApp/UltiTourneysApp/Migrations/20230725040815_AddPlayerEmailAddress.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UltiTourneysApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayerEmailAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Player",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Player",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Player");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Player");
+        }
+    }
+}
